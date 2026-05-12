da0588628a03313b7699c4411bef61d5c96c8782
HoHuynhTrung <hohuynhtrung2003@gmail.com>
2026-05-12 17:01:21 +0700

    build project Slidedim library

---
+++ src/slidedim.js
@@ -0,0 +1,49 @@
+function Slidedim(selector, options = {}) {
+  this.container = document.querySelector(selector);
+  if (!this.container) {
+    console.error(`Slidedim: Container "${selector}" not found!`);
+    return;
+  }
+
+  this.opt = Object.assign({}, options);
+  this.slides = Array.from(this.container.children);
+
+  this._init();
+}
+
+Slidedim.prototype._init = function () {
+  this.container.classList.add("slidedim-wrapper");
+  this._createTrack();
+  this._CreateNavigation();
+};
+
+Slidedim.prototype._createTrack = function () {
+  this.track = document.createElement("div");
+  this.track.className = "slidedim-track";
+
+  this.slides.forEach((slide) => {
+    slide.classList.add("slidedim-slide");
+    this.track.appendChild(slide);
+  });
+
+  this.container.appendChild(this.track);
+};
+
+Slidedim.prototype._CreateNavigation = function () {
+  this.prevbtn = document.createElement("button");
+  this.prevbtn.className = "slidedim-prev";
+  this.prevbtn.innerText = "<";
+
+  this.nextBtn = document.createElement("button");
+  this.nextBtn.className = "slidedim-next";
+  this.nextBtn.innerText = ">";
+
+  this.container.append(this.prevbtn, this.nextBtn);
+
+  this.prevbtn.onclick = () => this.moveSlide(-1);
+  this.nextBtn.onclick = () => this.moveSlide(1);
+};
+
+Slidedim.prototype.moveSlide = function (step) {
+  console.log(step);
+};
