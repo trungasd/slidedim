e547d2283a5e8f3305f69123bf49d161a0dabc5d
HoHuynhTrung <hohuynhtrung2003@gmail.com>
2026-05-13 20:09:47 +0700

    added navigation

--- src/slidedim.js
+++ src/slidedim.js
@@ -10,6 +10,7 @@ function Slidedim(selector, options = {}) {
       items: 0,
       speed: 0,
       loop: false,
+      nav: true,
     },
     options,
   );
@@ -22,8 +23,17 @@ function Slidedim(selector, options = {}) {
 
 Slidedim.prototype._init = function () {
   this.container.classList.add("slidedim-wrapper");
+  this._createContent();
   this._createTrack();
-  this._CreateNavigation();
+  this._createControls();
+  this._createNav();
+};
+
+Slidedim.prototype._createContent = function () {
+  this.content = document.createElement("div");
+  this.content.className = "slidedim-content";
+
+  this.container.appendChild(this.content);
 };
 
 Slidedim.prototype._createTrack = function () {
@@ -47,10 +57,10 @@ Slidedim.prototype._createTrack = function () {
     this.track.appendChild(slide);
   });
 
-  this.container.appendChild(this.track);
+  this.content.appendChild(this.track);
 };
 
-Slidedim.prototype._CreateNavigation = function () {
+Slidedim.prototype._createControls = function () {
   this.prevbtn = document.createElement("button");
   this.prevbtn.className = "slidedim-prev";
   this.prevbtn.innerText = "<";
@@ -59,10 +69,37 @@ Slidedim.prototype._CreateNavigation = function () {
   this.nextBtn.className = "slidedim-next";
   this.nextBtn.innerText = ">";
 
-  this.container.append(this.prevbtn, this.nextBtn);
+  this.content.append(this.prevbtn, this.nextBtn);
 
   this.prevbtn.onclick = () => this.moveSlide(-1);
   this.nextBtn.onclick = () => this.moveSlide(1);
+};
+
+Slidedim.prototype._createNav = function () {
+  this.navWrapper = document.createElement("div");
+  this.navWrapper.className = "slidedim-nav";
+
+  const slideCount =
+    this.slides.length - (this.opt.loop ? this.opt.items * 2 : 0);
+  const pageCount = Math.ceil(slideCount / this.opt.items);
+
+  for (let i = 0; i < pageCount; i++) {
+    const dot = document.createElement("button");
+    dot.className = "slidedim-dot";
+
+    if (dot === 0) dot.className = "active";
+
+    dot.onclick = () => {
+      this.currentIndex = this.opt.loop
+        ? i * this.opt.items + this.opt.items
+        : i * this.opt.items;
+      this._updatePosition();
+    };
+
+    this.navWrapper.appendChild(dot);
+  }
+
+  this.container.appendChild(this.navWrapper);
 };
 
 Slidedim.prototype.moveSlide = function (step) {
@@ -76,15 +113,35 @@ Slidedim.prototype.moveSlide = function (step) {
     if (this.opt.loop) {
       if (this.currentIndex <= 0) {
         this.currentIndex = maxIndex - this.opt.items;
+        this._updatePosition(true);
       } else if (this.currentIndex >= maxIndex) {
         this.currentIndex = this.opt.items;
+        this._updatePosition(true);
       }
-      this._updatePosition(true);
     }
     this._isAnimating = false;
   }, this.opt.speed);
 
   this._updatePosition();
+};
+
+Slidedim.prototype._updateNav = function () {
+  let realIndex = this.currentIndex;
+
+  if (this.opt.loop) {
+    const slideCount = this.slides.length - this.opt.items * 2;
+    realIndex = (this.currentIndex - this.opt.items + slideCount) % slideCount;
+  }
+
+  const pageIndex = Math.floor(realIndex / this.opt.items);
+
+  console.log(pageIndex);
+
+  const dot = Array.from(this.navWrapper.children);
+
+  dot.forEach((dot, index) => {
+    dot.classList.toggle("active", index === pageIndex);
+  });
 };
 
 Slidedim.prototype._updatePosition = function (instant = false) {
@@ -93,4 +150,8 @@ Slidedim.prototype._updatePosition = function (instant = false) {
     : `transform ease ${this.opt.speed}ms`;
   this.offset = -(this.currentIndex * (100 / this.opt.items));
   this.track.style.transform = `translateX(${this.offset}%)`;
+
+  if (this.opt.nav && !instant) {
+    this._updateNav();
+  }
 };
